4ebae47f23affc67d8ecb0f8636d479253b4070e
Kain Noxfall <kain.noxfall@gmail.com>
2023-03-01 13:01:24 +0100

    HTML/CSS/JS Code Arena Finished

--- pages/htmlcssjs/index.tsx
+++ pages/htmlcssjs/index.tsx
@@ -1,25 +1,31 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Editor from '@/components/Editor';
 import Preview from '@/components/Preview';
 
 const HtmlCssJs: React.FC = () => {
     const [html, setHtml] = useState('');
-    const [doc, setDoc] = useState<string>('<h1>Hello, world!</h1>');
+    const [css, setCss] = useState('');
+    const [js, setJs] = useState('');
+    const [doc, setDoc] = useState<string>('');
 
     useEffect(() => {
         const timeout = setTimeout(() => {
             setDoc(`
                 <html>
                     <body>${html}</body>
+                    <style>${css}</style>
+                    <script>${js}</script>
                 </html>
             `);
         }, 250);
         return () => clearTimeout(timeout)
-    }, [html]);
+    }, [html, css, js]);
 
     return (
         <main>
             <Editor onChange={setHtml} initialDoc={doc} />
+            <Editor onChange={setCss} initialDoc={doc} />
+            <Editor onChange={setJs} initialDoc={doc} />
             <Preview doc={doc} />
         </main>
     );
